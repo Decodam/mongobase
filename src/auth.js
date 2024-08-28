155b415dbd01d10e1227a580bc2dfec59211b16e
Decodam <arghyamondal.work@gmail.com>
2024-08-28 16:26:20 +0530

    added role col

--- src/auth.js
+++ src/auth.js
@@ -13,7 +13,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-    Google
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
   pages: {
     signIn: "/login",
